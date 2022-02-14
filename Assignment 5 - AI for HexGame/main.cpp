fcbfde8b8809d9353a593f294b8fbad966e8e103
DragonAngel1st <31282296+DragonAngel1st@users.noreply.github.com>
2022-02-14 13:58:24 -0500

    Initial Commit

---
+++ Assignment 5 - AI for HexGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 5 - AI for HexGame
+//
+//  Created by Patrick Miron on 2022-02-14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
